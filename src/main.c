b1063a3a54e7ce23364fd0aeda645b61429bdba8
Vytautas Valaitis <vytautas@valaitis.net>
2018-02-06 22:22:06 +0200

    uart

--- src/main.c
+++ src/main.c
@@ -15,19 +15,24 @@ static THD_WORKING_AREA(waThread1, 128);
 
 static msg_t Thread1(void *arg) {
   (void)arg;
+
+  uint8_t buf = '.';
   chRegSetThreadName("blinker");
+
   while (TRUE) {
     palClearPad(GPIOB, GPIOB_LED_RED);
     palClearPad(GPIOB, GPIOB_LED_GREEN);
     chThdSleepMilliseconds(500);
     palSetPad(GPIOB, GPIOB_LED_RED);
     palSetPad(GPIOB, GPIOB_LED_GREEN);
+    uartStartSend(&UARTD1, 1, &buf);
     chThdSleepMilliseconds(500);
   }
 }
 
 int main(void) {
   halInit();
+  uartInit();
   chSysInit();
 
   chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
