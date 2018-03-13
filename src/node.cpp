b638a009cafd950aca5ae5387d62457c75730b4e
Matas Razgunas <matas.razgunas@gmail.com>
2018-03-13 19:11:35 +0200

    Add parameter server. Untested

--- src/node.cpp
+++ src/node.cpp
@@ -3,6 +3,10 @@
 #include <uavcan_stm32/uavcan_stm32.hpp>
 #include <uavcan/protocol/debug/KeyValue.hpp>
 
+#include <uavcan/protocol/param_server.hpp>
+#include <uavcan/protocol/restart_request_server.hpp>
+#include <config/config.hpp>
+
 #include <ch.hpp>
 
 namespace Node {
@@ -14,8 +18,124 @@ namespace Node {
     return node;
   }
 
+  /*
+   * Param access server
+   */
+  class ParamManager : public uavcan::IParamManager
+  {
+      void convert(float native_value, ConfigDataType native_type, Value& out_value) const
+      {
+          if (native_type == CONFIG_TYPE_BOOL)
+          {
+              out_value.to<Value::Tag::boolean_value>() = !uavcan::isCloseToZero(native_value);
+          }
+          else if (native_type == CONFIG_TYPE_INT)
+          {
+              out_value.to<Value::Tag::integer_value>() = static_cast<std::int64_t>(native_value);
+          }
+          else if (native_type == CONFIG_TYPE_FLOAT)
+          {
+              out_value.to<Value::Tag::real_value>() = native_value;
+          }
+          else
+          {
+              ; // Invalid type
+          }
+      }
+
+      void convert(float native_value, ConfigDataType native_type, NumericValue& out_value) const
+      {
+          if (native_type == CONFIG_TYPE_INT)
+          {
+              out_value.to<NumericValue::Tag::integer_value>() = static_cast<std::int64_t>(native_value);
+          }
+          else if (native_type == CONFIG_TYPE_FLOAT)
+          {
+              out_value.to<NumericValue::Tag::real_value>() = native_value;
+          }
+          else
+          {
+              ; // Not applicable
+          }
+      }
+
+      void getParamNameByIndex(Index index, Name& out_name) const override
+      {
+          const char* name = configNameByIndex(index);
+          if (name != nullptr)
+          {
+              out_name = name;
+          }
+      }
+
+      void assignParamValue(const Name& name, const Value& value) override
+      {
+          float native_value = 0.F;
+
+          if (value.is(Value::Tag::boolean_value))
+          {
+              native_value = (*value.as<Value::Tag::boolean_value>()) ? 1.F : 0.F;
+          }
+          else if (value.is(Value::Tag::integer_value))
+          {
+              native_value = static_cast<float>(*value.as<Value::Tag::integer_value>());
+          }
+          else if (value.is(Value::Tag::real_value))
+          {
+              native_value = *value.as<Value::Tag::real_value>();
+          }
+          else
+          {
+              return;
+          }
+
+          (void)configSet(name.c_str(), native_value);
+      }
+
+      void readParamValue(const Name& name, Value& out_value) const override
+      {
+          ConfigParam descr;
+          const int res = configGetDescr(name.c_str(), &descr);
+          if (res >= 0)
+          {
+              convert(configGet(name.c_str()), descr.type, out_value);
+          }
+      }
+
+      void readParamDefaultMaxMin(const Name& name, Value& out_default,
+                                  NumericValue& out_max, NumericValue& out_min) const override
+      {
+          ConfigParam descr;
+          const int res = configGetDescr(name.c_str(), &descr);
+          if (res >= 0)
+          {
+              convert(descr.default_, descr.type, out_default);
+              convert(descr.max, descr.type, out_max);
+              convert(descr.min, descr.type, out_min);
+          }
+      }
+
+      int saveAllParams() override
+      {
+          return configSave();
+      }
+
+      int eraseAllParams() override
+      {
+          return configErase();
+      }
+  } param_manager;
+
+  class RestartRequestHandler: public uavcan::IRestartRequestHandler {
+    bool handleRestartRequest(uavcan::NodeID request_source) override
+    {
+      NVIC_SystemReset();
+      return true;
+    }
+  } restart_request_handler;
+
   void uavcanNodeThread::main() {
-    uavcan::uint32_t bitrate = 1000000;
+    uavcan::uint32_t bitrate = 100000;
     can.init(bitrate);
 
     getNode().setName("org.kmti.gmm_controler");
@@ -28,6 +148,11 @@ namespace Node {
     uavcan::Publisher<uavcan::protocol::debug::KeyValue> kv_pub(getNode());
     kv_pub.init();
     uavcan::protocol::debug::KeyValue kv_msg;
+
+    getNode().setRestartRequestHandler(&restart_request_handler);
+
+    uavcan::ParamServer server(getNode());
+    server.start(&param_manager);
 
     getNode().setModeOperational();
 
